06d34ad6c762881a37ca5d2fb30e8e88957ee928
Ana Skarica <ana.skarica.as@gmail.com>
2023-03-01 21:44:33 +0100

    add mobile navigacija v1

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import NavbarDesktop from "./Navbar-desktop";
+import NavbarMobile from "./Navbar-mobile";
 
 const Header = () => {
+  // TODO smanji ovo
+  const [isDesktop, setDesktop] = useState(window.innerWidth > 768);
+
+  const updateMedia = () => {
+    setDesktop(window.innerWidth > 650);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateMedia);
+    return () => window.removeEventListener("resize", updateMedia);
+  });
+
   return (
     <div className="header">
       <div className="header-items">
-        <Link to="/">
+        {isDesktop ? (
+          <div className="desktop">
+            <NavbarDesktop />
+          </div>
+        ) : (
+          <div className="mobile">
+            <NavbarMobile />
+          </div>
+        )}
+        <Link to="/" className="image">
           {" "}
           <img src={require("../Images/logo_marina.png")} alt="logo-marina" />
         </Link>
-        <NavbarDesktop />
       </div>
     </div>
   );
---
+++ src/Components/Navbar-mobile.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { faX, faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
+
+const NavbarMobile = () => {
+  const [openNav, setOpenNav] = useState(false);
+  const navHandler = () => {
+    setOpenNav(!openNav);
+  };
+
+  return (
+    <div className="navigation-mobile">
+      <div className="icons">
+        <FontAwesomeIcon
+          icon={faBars}
+          onClick={navHandler}
+          className={openNav ? "hidden" : "iconBar fixed"}
+        />
+        <FontAwesomeIcon
+          icon={faX}
+          onClick={navHandler}
+          className={openNav ? "iconBar abs" : "hidden"}
+        />
+      </div>
+      <div>
+        <nav className={openNav ? "nav-bar" : "hidden "}>
+          <Link to="/">Pocetna</Link>
+          <Link to="/oapartmanu">O apartmanu</Link>
+          <Link to="/cjenik">Cjenik</Link>
+          <Link to="/omjestu">O mjestu</Link>
+          <Link to="/kontakt">Kontakt</Link>
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default NavbarMobile;
