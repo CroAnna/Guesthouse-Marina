9bbcc1e3051184808d2ab879a3fb28a07874fb39
Ana Skarica <ana.skarica.as@gmail.com>
2023-03-14 09:29:46 +0100

    add prijevod na atrakcije

--- src/Components/Attraction.jsx
+++ src/Components/Attraction.jsx
@@ -1,13 +1,15 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 
 const Attraction = ({ attraction }) => {
+  const { t } = useTranslation();
   return (
     <div className="attraction">
       <a href={attraction.url}>
         <div className="text ">
           <div className="move-up-middle inside">
-            <h2>{attraction.title}</h2>
-            <p>{attraction.description}</p>
+            <h2>{t(`${attraction.title}`)}</h2>
+            <p>{t(`${attraction.description}`)}</p>
           </div>
         </div>
         <div className="image">
--- src/Data/KarlovacAttractionsList.js
+++ src/Data/KarlovacAttractionsList.js
@@ -1,62 +1,62 @@
 export const KarlovacAttractions = [
     {
-        title: "STARI GRAD DUBOVAC",
-        description: 'Stari grad Dubovac - dvorac iz 13. stoljeća na brijegu s kojeg se pruža prekrasan pogled na grad. Ime je dobio po hrastu (dubu) jer je njime okružen, a danas se u njemu nalazi restoran.',
+        title: "dubovac",
+        description: 'dubovac_desc',
         imageName: "dubovac",
         url: "https://visitkarlovac.hr/stari-grad-dubovac/",
         used: true
     }, {
-        title: "ZVIJEZDA",
-        description: 'Stara gradska jezgra, mjesto rođenja grada Karlovca 13. srpnja 1579. godine, najpoznatiji je dio cijelog grada te ono po čemu je Karlovac prepoznatljiv - šesterokrakoj zvijezdi. U samome središtu, nalazi se Trg bana Josipa Jelačića i zdenac, okružen crkvama Presvetog Trojstva i svetog Nikole.',
+        title: "zvijezda",
+        description: 'zvijezda_desc',
         imageName: "zvijezda",
         url: "https://visitkarlovac.hr/zvijezda/",
         used: true
     }, {
-        title: "ŽITNA LADA",
-        description: 'Lađarstvo je u Karlovcu bilo od značajne važnosti kroz povijest. Opskrbljivalo je stanovništvo i vojsku te povezivalo gradove, a danas služi kao turistička atrakcija s druge strane Kupe, udaljeno od nas svega 11 km. Provozajte se na tumbas lađi, primjerenoj za obiteljske izlete. ',
+        title: "grain_boat",
+        description: 'grain_desc',
         imageName: "zitna",
         url: "https://visitkarlovac.hr/zitna-lada/",
         used: true
     }, {
-        title: "AQUATIKA - SLATKOVODNI AKVARIJ",
-        description: 'Jedini potpuno slatkovodni akvarij u Hrvatskoj, osnovan u svrhu edukacije i podizanja svijesti o važnosti biološke raznolikosti. Smješten je u Karlovcu, na desnoj obali rijeke Korane, a inspiracija za dizajn je karlovačka zvijezda okružena bedemima.',
+        title: "aquatika",
+        description: 'aquatika_desc',
         imageName: "aquatika",
         url: "https://visitkarlovac.hr/aquatika/",
         used: true
 
     }, {
-        title: "4 RIJEKE",
-        description: 'Karlovac se naziva gradom na četiri rijeke, a sve je u gradu povezano s pojmom rijeka. Kupa, Korana, Dobra i Mrežnica. Svaka od njih ima ono nešto svoje, a u svakoj se možete okupati u vrućem ljetnom danu.',
+        title: "four_rivers",
+        description: 'four_rivers_desc',
         imageName: "rijeke",
         url: "https://visitkarlovac.hr/cetiri-rijeke/",
         used: true
     }, {
-        title: "DANI PIVA",
-        description: 'Najpoznatiji i najdugovječniji hrvatski festival piva i gastronomije, a ujedno i prestižni glazbeni festival koji se održava od 1984. godine. Tijekom njegovog trajanja povijesne karlovačke ulice postaju pozornice i restorani, a grad je prepun raznolikih sadržaja.',
+        title: "dani_piva",
+        description: 'dani_piva_desc',
         imageName: "danipiva",
         url: "https://danipiva.net/",
         used: true
     }, {
-        title: "MUZEJ DOMOVINSKOG RATA",
-        description: 'Gradska četvrt Turanj bio je strateška točka važna za obranu grada u Domovinskom ratu 1991.-1995. Ono je mjesto gdje je zaustavljen neprijatelj u pokušaju zauzimanja grada čime bi se presjekla Hrvatska. Turanj je danas simbol pobjede i borbe za samostalnost Hrvatske. ',
+        title: "homeland_war_museum",
+        description: 'homeland_war_museum_desc',
         imageName: "muzejturanj",
         url: "https://visitkarlovac.hr/muzej-domovinskog-rata/",
         used: true
     }, {
-        title: "GRAD PARKOVA",
-        description: 'Karlovac ima mnoštvo parkova, a prvi su planirani već pri samoj gradnji utvrde. Prošećite njima i uživajte u cvrkutu ptica, okruženi biljkama i rijekama. Neki od parkova su Velika promenada, parkovni nasadi u šanćevima i Modrušanov park. Ostale otkrijte sami...',
+        title: "city_of_parks",
+        description: 'city_of_parks_desc',
         imageName: "parkovi",
         url: "https://visitkarlovac.hr/parkovi-i-perivoji/",
         used: true
     }, {
-        title: "ZVJEZDANO LJETO",
-        description: 'Manifestacija koja se održava početkom ljeta - započinje Ivanjskim krijesom na obalama dvaju gradskih četvtri - Banije i Gaze i traje sve do 13. srpnja, rođendana grada Karlovca. Obilježava ga mnoštvo događaja, koncerata i aktivnosti koje se prostiru kroz centar grada.',
+        title: "star_summer",
+        description: 'star_summer_desc',
         imageName: "zvjezdanoljeto",
         url: "https://zvjezdanoljeto.karlovac.hr/",
         used: true
     }, {
-        title: "NACIONALNO SVETIŠTE SVETOG JOSIPA",
-        description: 'Zaštitnik grada Karlovca jest sveti Josip te je dotadašnja crkva na Dubovcu postala nacionalnim svetištem, a danas se ondje kroz godinu okuplja velik broj hodočasnika. ',
+        title: "nat_shr_st_joseph",
+        description: 'nat_shr_st_joseph_desc',
         imageName: "nacsvetiste",
         url: "https://www.svetijosip.com/",
         used: true
