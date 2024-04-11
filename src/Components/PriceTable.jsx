6ab17fad62e396d2c407b1fe3338781750efc402
Ana Škarica <ana.skarica.as@gmail.com>
2024-04-11 11:24:18 +0200

    Add new prices, breakfast column in price table

--- src/Components/PriceTable.jsx
+++ src/Components/PriceTable.jsx
@@ -3,36 +3,50 @@ import "aos/dist/aos.css";
 import React, { useEffect } from "react";
 import { useTranslation } from "react-i18next";
 
-const PriceTable = () => {
+const PriceTable = ({
+  price1,
+  price2,
+  price3,
+  price4,
+  price5,
+  price6,
+  price7,
+  price8,
+}) => {
   useEffect(() => {
     Aos.init();
   }, []);
   const { t } = useTranslation();
   return (
-    <div data-aos="fade-right">
+    <div data-aos="fade-right" className="price-table-container">
       <table>
         <thead>
           <tr>
             <th>{t("number_of_guests")}</th>
             <th>{t("price_per_night")} *</th>
+            <th>{t("price_per_night_w_breakfast")} *</th>
           </tr>
         </thead>
         <tbody>
           <tr>
             <td>1</td>
-            <td>67,50 €</td>
+            <td>{price1}</td>
+            <td>{price5}</td>
           </tr>
           <tr>
             <td>2</td>
-            <td>75,00 €</td>
+            <td>{price2}</td>
+            <td>{price6}</td>
           </tr>
           <tr>
             <td>3</td>
-            <td>90,00 €</td>
+            <td>{price3}</td>
+            <td>{price7}</td>
           </tr>
           <tr>
             <td>4</td>
-            <td>90,00 €</td>
+            <td>{price4}</td>
+            <td>{price8}</td>
           </tr>
         </tbody>
       </table>
--- src/Pages/Cjenik.jsx
+++ src/Pages/Cjenik.jsx
@@ -17,7 +17,33 @@ const Cjenik = () => {
         <h1>{t("pricelist-nav")}</h1>
         <hr className="short" />
       </div>
-      <PriceTable />
+      <h3 className="title">
+        Od/from 10.4.2024. do/to 31.5.2024. Od/from 1.10.2024. do/to 31.12.2024.
+        :
+      </h3>
+      <PriceTable
+        price1={"60,00 €"}
+        price2={"75,00 €"}
+        price3={"90,00 €"}
+        price4={"105,00 €"}
+        price5={"65,00 €"}
+        price6={"85,00 €"}
+        price7={"105,00 €"}
+        price8={"125,00 €"}
+      />
+
+      <h3 className="title">Od/from 1.6.2024. do/to 30.9.2024. :</h3>
+      <PriceTable
+        price1={"70,00 €"}
+        price2={"85,00 €"}
+        price3={"100,00 €"}
+        price4={"115,00 €"}
+        price5={"75,00 €"}
+        price6={"95,00 €"}
+        price7={"115,00 €"}
+        price8={"135,00 €"}
+      />
+
       <div className="text-info" data-aos="fade-left">
         <p>{t("pricelist_info_1")}</p>
         <p>
